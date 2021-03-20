8d8c20e32f6d97b4ce286dfe17572847f5f3ce9d
Nicolas <you@example.com>
2021-03-20 17:10:12 -0300

    tercer commit. Ejercicio 1 terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ejercicio1';
+  title = 'Ejercicio 1';
+  edadUno:number;
+  edadDos:number;
+  suma:number;
+  promedio:number;
+
+  LimpiarDatos(){
+    this.edadUno= 0;
+    this.edadDos= 0;
+    this.suma = 0;
+    this.promedio = 0;
+  }
+
+  Calcular(){
+    this.suma = this.edadUno + this.edadDos;
+    this.promedio = this.suma/2;
+  }
+
 }
